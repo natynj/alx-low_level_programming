bd3f457e4a45376f821b44c3efeef5d5bc77bb23
natynj <natnaelhailu16@gmail.com>
2023-07-13 19:12:03 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<studio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("ith proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
